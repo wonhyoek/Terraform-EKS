6d29b689fa56177dc4c7045850134acaf6e9260f
wonhyoek <epahsejtmxm@naver.com>
2023-01-12 22:23:36 +0900

    Add provider and terraform config

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform" // credential
+  region = "us-east-1"
+}
+
